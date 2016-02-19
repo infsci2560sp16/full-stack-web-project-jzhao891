954ef8b4e9de3bcf94d0374804c2198136de129f
jzhao891 <jzhao891@gmail.com>
2016-02-18 19:33:43 -0500

    Zexin Zhao

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,8 +13,8 @@ import static spark.Spark.get;
 
 import com.heroku.sdk.jdbc.DatabaseUrl;
 
-import static javax.measure.unit.SI.KILOGRAM;
-import javax.measure.quantity.Mass;
+//import static javax.measure.unit.SI.KILOGRAM;
+//import javax.measure.*;
 import org.jscience.physics.model.RelativisticModel;
 import org.jscience.physics.amount.Amount;
 
@@ -24,51 +24,22 @@ public class Main {
 
     port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
+    get("/hello", (req, res) -> {
+      RelativisticModel.select();
+      Amount<Mass> m = Amount.valueOf("12 GeV").to(KILOGRAM);
+      return "E=mc^2: 12 GeV = " + m.toString();
+    });
 
-    get("/hello", (req, res) -> "Hello World");
-      get("/hello", (req, res) -> {
-          RelativisticModel.select();
-          
-          String energy = System.getenv().get("ENERGY");
-          
-          Amount<Mass> m = Amount.valueOf(energy).to(KILOGRAM);
-          return "E=mc^2: " + energy + " = " + m.toString();
-      });
+    //get("/hello", (req, res) -> "Hello World");
 
     get("/", (request, response) -> {
             Map<String, Object> attributes = new HashMap<>();
             attributes.put("message", "Hello World!");
 
             return new ModelAndView(attributes, "index.ftl");
+        }, new FreeMarkerEngine());
 
-    }, new FreeMarkerEngine());
-
-    /*get("/db", (req, res) -> {
-      Connection connection = null;
-      Map<String, Object> attributes = new HashMap<>();
-      try {
-        connection = DatabaseUrl.extract().getConnection();
-
-        Statement stmt = connection.createStatement();
-        stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-        stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-        ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-
-        ArrayList<String> output = new ArrayList<String>();
-        while (rs.next()) {
-          output.add( "Read from DB: " + rs.getTimestamp("tick"));
-        }
-
-        attributes.put("results", output);
-        return new ModelAndView(attributes, "db.ftl");
-      } catch (Exception e) {
-        attributes.put("message", "There was an error: " + e);
-        return new ModelAndView(attributes, "error.ftl");
-      } finally {
-        if (connection != null) try{connection.close();} catch(SQLException e){}
-      }
-    }*/
-       get("/db", (req, res) -> {
+    get("/db", (req, res) -> {
       Connection connection = null;
       Map<String, Object> attributes = new HashMap<>();
       try {
